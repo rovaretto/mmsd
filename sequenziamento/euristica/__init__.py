34fd215297e83853dcf44712fb9ab2d9a460e39e
Samuele Perrotta <samuele.perrotta@edu.unito.it>
2024-05-19 23:30:09 +0200

    dovrebbe andare

--- sequenziamento/euristica/__init__.py
+++ sequenziamento/euristica/__init__.py
@@ -1,19 +1,13 @@
 import numpy as np
 
-vetSalaA = np.array([5.0, 10.0, 14.0, 3.0])
-vetSalaB = np.array([14.0, 11.0, 7.0])   # [7.0, 14.0, 11.0]
-                                         # []
-vetSalaC = np.array([2.0, 4.0, 7.0, 9.0, 10.0])
-vetSalaD = np.array([20.0, 12.0])
-
-# vetSalaA = np.array([3.0, 8.0, 18.0, 32.0])  [5.0, 15.0, 29.0, 32.0]
-# vetSalaB = np.array([14.0, 25.0, 32.0])      [7.0, 21.0, 32.0]
-# vetSalaC = np.array([2.0, 6.0, 13.0, 22.0, 32.0])
-# vetSalaD = np.array([20.0, 32.0])
+vetSalaA = np.array([5.0, 3.0, 14.0, 10.0])
+vetSalaB = np.array([14.0, 7.0, 11.0])
+vetSalaC = np.array([7.0, 4.0, 9.0, 2.0, 10.0])
+vetSalaD = np.array([12.0, 20.0])
 
 vetSalaA.sort()
-# vetSalaB.sort()
-# vetSalaB = vetSalaB[::-1]
+vetSalaB.sort()
+vetSalaB = vetSalaB[::-1]
 vetSalaC.sort()
 vetSalaD.sort()
 vetSalaD = vetSalaD[::-1]
@@ -46,85 +40,104 @@ def ultimoAllInizio(vetSala):
     vetSala = np.roll(vetSala,1)
     return(vetSala)
 
-def durataInTempoFine(salaA, salaB, salaC, salaD):
-    copiaSalaA = salaA
+def durataInTempoFineA(salaA):
+    copiaSalaA = salaA.copy()
     i = 1
     while i < len(salaA):
         copiaSalaA[i] = copiaSalaA[i-1] + salaA[i]
         i += 1
+    return(copiaSalaA)
 
+def durataInTempoFineB(salaB):
+    copiaSalaB = salaB.copy()
+    i = 1
+    while i < len(salaB):
+        copiaSalaB[i] = copiaSalaB[i-1] + salaB[i]
+        i += 1
+    return(copiaSalaB)
 
-
-    copiaSalaC = salaC
+def durataInTempoFineC(salaC):
+    copiaSalaC = salaC.copy()
     i = 1
     while i < len(salaC):
-        copiaSalaC[i] = copiaSalaC[i - 1] + salaC[i]
+        copiaSalaC[i] = copiaSalaC[i-1] + salaC[i]
         i += 1
+    return(copiaSalaC)
 
-    return(copiaSalaA, copiaSalaB, copiaSalaC, copiaSalaD)
+def durataInTempoFineD(salaD):
+    copiaSalaD = salaD.copy()
+    i = 1
+    while i < len(salaD):
+        copiaSalaD[i] = copiaSalaD[i-1] + salaD[i]
+        i += 1
+    return(copiaSalaD)
 
-print(vetSalaA)
-print(durataInTempoFine(vetSalaA))
+biiMaxIniziale = biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+print("BII max iniziale", biiMaxIniziale, "con questa configurazione:", "Sala A", copiaVetSalaA, "Sala B", copiaVetSalaB, "Sala C", copiaVetSalaC, "Sala D", copiaVetSalaD)
+while 1:
+    if calcolaBII(concatenaEordina(durataInTempoFineA(primoAllaFine(copiaVetSalaA)), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD))) < biiMax:
+        copiaVetSalaA = primoAllaFine(copiaVetSalaA)
+        biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+        if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(ultimoAllInizio(copiaVetSalaB)), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD))) < biiMax:
+            copiaVetSalaB = ultimoAllInizio(copiaVetSalaB)
+            biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+            if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(primoAllaFine(copiaVetSalaC)), durataInTempoFineD(copiaVetSalaD))) < biiMax:
+                copiaVetSalaC = primoAllaFine(copiaVetSalaC)
+                biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+                if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(ultimoAllInizio(copiaVetSalaD)))) < biiMax:
+                    copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+            else:
+                if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(ultimoAllInizio(copiaVetSalaD)))) < biiMax:
+                    copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+        else:
+            if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(primoAllaFine(copiaVetSalaC)), durataInTempoFineD(copiaVetSalaD))) < biiMax:
+                copiaVetSalaC = primoAllaFine(copiaVetSalaC)
+                biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+                if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(ultimoAllInizio(copiaVetSalaD)))) < biiMax:
+                    copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+            else:
+                if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(ultimoAllInizio(copiaVetSalaD)))) < biiMax:
+                    copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+    else:
+        if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(ultimoAllInizio(copiaVetSalaB)), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD))) < biiMax:
+            copiaVetSalaB = ultimoAllInizio(copiaVetSalaB)
+            biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+            if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(primoAllaFine(copiaVetSalaC)), durataInTempoFineD(copiaVetSalaD))) < biiMax:
+                copiaVetSalaC = primoAllaFine(copiaVetSalaC)
+                biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+                if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(ultimoAllInizio(copiaVetSalaD)))) < biiMax:
+                    copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+            else:
+                if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(ultimoAllInizio(copiaVetSalaD)))) < biiMax:
+                    copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+        else:
+            if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(primoAllaFine(copiaVetSalaC)), durataInTempoFineD(copiaVetSalaD))) < biiMax:
+                copiaVetSalaC = primoAllaFine(copiaVetSalaC)
+                biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+                if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(ultimoAllInizio(copiaVetSalaD)))) < biiMax:
+                    copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+            else:
+                if calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(ultimoAllInizio(copiaVetSalaD)))) < biiMax:
+                    copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+                else:
+                    biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+                    print("BII max finale:", biiMax, "con questa configurazione:", "Sala A", copiaVetSalaA, "Sala B", copiaVetSalaB, "Sala C", copiaVetSalaC, "Sala D", copiaVetSalaD)
+                    break
 
+    if (copiaVetSalaA == vetSalaA).all() and (copiaVetSalaB == vetSalaB).all() and (copiaVetSalaC == vetSalaC).all() and (copiaVetSalaD == vetSalaD).all():
+        biiMax = calcolaBII(concatenaEordina(durataInTempoFineA(copiaVetSalaA), durataInTempoFineB(copiaVetSalaB), durataInTempoFineC(copiaVetSalaC), durataInTempoFineD(copiaVetSalaD)))
+        print("BII max finale:", biiMax, "con questa configurazione:", "Sala A", copiaVetSalaA, "Sala B", copiaVetSalaB, "Sala C", copiaVetSalaC, "Sala D", copiaVetSalaD)
+        break
 
-# biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-# print("BII max iniziale", biiMax)
-# while 1:
-#     if calcolaBII(concatenaEordina(primoAllaFine(copiaVetSalaA), copiaVetSalaB, copiaVetSalaC, copiaVetSalaD)) < biiMax:
-#         copiaVetSalaA = primoAllaFine(copiaVetSalaA)
-#         biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#         if calcolaBII(concatenaEordina(copiaVetSalaA, ultimoAllInizio(copiaVetSalaB), copiaVetSalaC, copiaVetSalaD)) < biiMax:
-#             copiaVetSalaB = ultimoAllInizio(copiaVetSalaB)
-#             biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#             if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, primoAllaFine(copiaVetSalaC), copiaVetSalaD)) < biiMax:
-#                 copiaVetSalaC = primoAllaFine(copiaVetSalaC)
-#                 biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#                 if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, ultimoAllInizio(copiaVetSalaD))) < biiMax:
-#                     copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
-#                     biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#             else:
-#                 if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, ultimoAllInizio(copiaVetSalaD))) < biiMax:
-#                     copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
-#                     biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#         else:
-#             if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, primoAllaFine(copiaVetSalaC), copiaVetSalaD)) < biiMax:
-#                 copiaVetSalaC = primoAllaFine(copiaVetSalaC)
-#                 biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#                 if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, ultimoAllInizio(copiaVetSalaD))) < biiMax:
-#                     copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
-#                     biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#             else:
-#                 if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, ultimoAllInizio(copiaVetSalaD))) < biiMax:
-#                     copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
-#                     biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#     else:
-#         if calcolaBII(concatenaEordina(copiaVetSalaA, ultimoAllInizio(copiaVetSalaB), copiaVetSalaC, copiaVetSalaD)) < biiMax:
-#             copiaVetSalaB = ultimoAllInizio(copiaVetSalaB)
-#             biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#             if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, primoAllaFine(copiaVetSalaC), copiaVetSalaD)) < biiMax:
-#                 copiaVetSalaC = primoAllaFine(copiaVetSalaC)
-#                 biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#                 if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, ultimoAllInizio(copiaVetSalaD))) < biiMax:
-#                     copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
-#                     biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#             else:
-#                 if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, ultimoAllInizio(copiaVetSalaD))) < biiMax:
-#                     copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
-#                     biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#         else:
-#             if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, primoAllaFine(copiaVetSalaC), copiaVetSalaD)) < biiMax:
-#                 copiaVetSalaC = primoAllaFine(copiaVetSalaC)
-#                 biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#                 if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, ultimoAllInizio(copiaVetSalaD))) < biiMax:
-#                     copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
-#                     biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#             else:
-#                 if calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, ultimoAllInizio(copiaVetSalaD))) < biiMax:
-#                     copiaVetSalaD = ultimoAllInizio(copiaVetSalaD)
-#                     biiMax = calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD))
-#                 else:
-#                     print("Nessun miglioramento!!!")
-#
-#     if (copiaVetSalaA == vetSalaA).all() and (copiaVetSalaB == vetSalaB).all() and (copiaVetSalaC == vetSalaC).all() and (copiaVetSalaD == vetSalaD).all():
-#         print("BII max finale:", calcolaBII(concatenaEordina(copiaVetSalaA, copiaVetSalaB, copiaVetSalaC, copiaVetSalaD)))
-#         break
+if biiMaxIniziale != biiMax:
+    print("BII MAX MIGLIORATO!!!")
+else:
+    print("BII max NON migliorato!!!")
